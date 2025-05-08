4f8e9bf40a462912cdff2bf0249570d234f8137b
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-05-08 20:51:28 +0100

    Added IntCollections class

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IntCollections
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void IntCollections()
         {
-            Console.WriteLine("Hello LP1!");
+            List<int> List = new List<int> {1, 10, -30, 10, -5};
+            Stack<int> Stack = new Stack<int> {1, 10, -30, 10, -5};
+            Queue<int> Queue = new Queue<int> {1, 10, -30, 10, -5};
+            HashSet<int> HashSet = new HashSet<int> {1, 10, -30, 10, -5};
+
+            foreach (int number in List)
+            {
+                Console.WriteLine(number);
+            }
+
+            foreach (int number in Stack)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }

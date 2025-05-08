9c6fbd39169db5f39505660a691369d226a4dc8e
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-05-08 19:08:20 +0100

    Added IntCollections project

---
+++ IntCollections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntCollections
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}

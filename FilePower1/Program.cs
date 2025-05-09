2b3205e938bbb3517f92e6a1cf6f43b19de9a82c
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-05-10 00:26:52 +0100

    Added FilePower1 project

---
+++ FilePower1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilePower1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}

9ac242860f89ed919d8d84d037af2d03f5fddd95
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-05-09 16:58:14 +0100

    Added IntGrouper project

---
+++ IntGrouper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntGrouper
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
